f08825594cd6320d5f82fac016dbd2e75fd557f9
Raquel Santos <raquel.pri@hotmail.com>
2024-04-18 17:02:36 -0300

    feat: creates account entity logic

--- src/com/raquel/banking/entities/Account.java
+++ src/com/raquel/banking/entities/Account.java
@@ -1,4 +1,30 @@
 package com.raquel.banking.entities;
 
 public class Account {
+    private Bank bank;
+    private double balance;
+
+    public Account(Bank bank) {
+        this.bank = bank;
+        this.balance = 0.0;
+    }
+
+    public Account(Bank bank, double balance) {
+        this.bank = bank;
+        this.balance = balance;
+    }
+
+    public synchronized double getBalance() {
+        return balance;
+    }
+
+    public synchronized void deposit(double amount) {
+        balance += amount;
+    }
+
+    public synchronized void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        }
+    }
 }
